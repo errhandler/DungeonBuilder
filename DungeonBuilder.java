26b08365e825df6c1bdc208e81e382b3d4eb212e
Tim Parker <tim@virtuallyabstract.net>
2012-02-11 18:38:59 -0600

    Increment version to 0.9.2

--- DungeonBuilder.java
+++ DungeonBuilder.java
@@ -197,7 +197,7 @@ public class DungeonBuilder extends JavaPlugin
 
 	@Override public void onEnable()
 	{
-		myLogger.log(Level.INFO, "DungeonBuilder (v.0.9.1) Enabled");
+		myLogger.log(Level.INFO, "DungeonBuilder (v.0.9.2) Enabled");
 
 		server = this.getServer();
 
